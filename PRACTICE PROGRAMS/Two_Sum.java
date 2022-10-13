a71f4aace4bf088d08985defb2dedc03ea36ff84
Balaji Sundar <94917311+BalajiSundar28@users.noreply.github.com>
2022-10-13 18:00:56 +0530

    Create Two_Sum.java

---
+++ PRACTICE PROGRAMS/Two_Sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Two_Sum {
+	public static void main (String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a, b;
+		System.out.println("input first and second numbers : ");
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		System.out.println("sum of " +a+ " and " +b+ " is " +(a+b));
+	}
+
+}
