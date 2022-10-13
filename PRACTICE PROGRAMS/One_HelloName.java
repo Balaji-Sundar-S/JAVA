b8d18aed420d175ca6b98f8b54f3ae2e26bbf563
Balaji Sundar <94917311+BalajiSundar28@users.noreply.github.com>
2022-10-13 17:55:05 +0530

    Create One_HelloName.java

---
+++ PRACTICE PROGRAMS/One_HelloName.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class One_HelloName {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("enter your name : ");
+		String a = sc.next();
+		System.out.println("Hello " +a);
+}
+
+}
